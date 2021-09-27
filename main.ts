46005d461f1dfecebecddda16bbdb4b09d4bc42f
Mickael <40719970+Starius-Project@users.noreply.github.com>
2021-09-27 19:09:55 +0200

    add done variable

--- main.ts
+++ main.ts
@@ -1,23 +1,28 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 5) {
+        servos.P0.setAngle(85)
+        done = 1
+        basic.showIcon(IconNames.Cow)
+    }
+})
 input.onButtonPressed(Button.A, function () {
     servos.P0.setAngle(85)
     basic.showString("O")
-})
-radio.onReceivedString(function (receivedString) {
-    if (receivedString == "up") {
-        servos.P0.setAngle(85)
-        basic.showIcon(IconNames.Cow)
-    }
 })
 input.onButtonPressed(Button.B, function () {
     servos.P0.setAngle(20)
     basic.showString("C")
 })
+let done = 0
 radio.setGroup(1)
 radio.setTransmitPower(7)
 basic.showString("FUEL BEACON")
 servos.P0.setAngle(20)
+done = 0
 basic.forever(function () {
-    radio.sendString("2")
-    basic.showString("F")
-    basic.pause(200)
+    if (done == 0) {
+        radio.sendString("2")
+        basic.showString("F")
+        basic.pause(200)
+    }
 })
